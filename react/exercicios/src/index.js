503505972b1e1f1fa012788ba8eed0223c718d20
durvallins <durval.lsiqueira@gmail.com>
2021-12-11 12:54:22 -0300

    confirm

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-import Saudacao from './componentes/Saudacao'
-
-import BomDia from './componentes/BomDia'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Saudacao tipo="Bom dia" nome="João" />
-        
-    </div>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
+        </Pai>
+    </div>    
 , document.getElementById('root'))
-
